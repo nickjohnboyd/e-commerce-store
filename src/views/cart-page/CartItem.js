ac74e8297c84ca1251e2515d9ba690ac36398d8b
Nick Boyd <nickjohnboyd@gmail.com>
2020-01-27 19:58:38 -0700

    changes to cart page

--- src/views/cart-page/CartItem.js
+++ src/views/cart-page/CartItem.js
@@ -7,10 +7,12 @@ class CartItem extends React.Component {
 	render() {
 		return (
 			<div className="CartItem">
-				<div className="item-img-cont">
-					<img className="item-img" src={this.props.img} alt="cart-item"></img>
+				<div className="item-left">
+					<div className="item-img-cont">
+						<img className="item-img" src={this.props.img} alt="cart-item"></img>
+					</div>
+					<div className="item-title">{this.props.title}</div>
 				</div>
-				<div className="item-title">{this.props.title}</div>
 				<div className="item-price">{this.props.price}</div>
 			</div>
 		);
