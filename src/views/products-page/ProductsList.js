337c74e4f7f3f710e6eff7141ce2df0c7a317c10
Nick Boyd <nickjohnboyd@gmail.com>
2020-04-09 17:43:46 -0600

    product filter finished

--- src/views/products-page/ProductsList.js
+++ src/views/products-page/ProductsList.js
@@ -3,9 +3,43 @@ import store from '../../store/index';
 import ProductItem from './ProductItem';
 
 class ProductsList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			products: [],
+			productItems: [],
+			filteredProducts: []
+		}
+	}
+	setFilter = (e) => {
+		console.log(e.target.value);
+		let filterVal = e.target.value;
+		let products = store.getState().products;
+		let filteredProducts = products.filter(p => {
+			return p.title.toLowerCase().includes(filterVal.toLowerCase()) === true;
+		});
+		console.log('filteredProducts');
+		console.log(filteredProducts);
+
+		this.setState({
+			filteredProducts: filteredProducts
+		}, this.buildProducts);
+	}
 	buildProducts = () => {
-		const products = store.getState().products;
-		return products.map((product, idx) => (
+		let myProducts = [];
+
+		if(this.state.filteredProducts.length > 0) {
+			myProducts = this.state.filteredProducts;
+		} else {
+			myProducts = store.getState().products;
+		}
+		this.setState({
+			products: myProducts
+		}, this.createProductItems);
+	}
+	
+	createProductItems = () => {
+		const productItems = this.state.products.map((product, idx) => (
 			<ProductItem
 				id={product.id}
 				title={product.title}
@@ -17,11 +51,27 @@ class ProductsList extends React.Component {
 				key={idx}
 			/>
 		));
+		let newState = this.state;
+		newState.productItems = productItems;
+		this.setState(newState);
+
+		console.log('state');
+		console.log(this.state);
+	}
+	componentDidMount() {
+		this.buildProducts();
 	}
 	render() {
 		return (
-			<div className="ProductsList">
-				{this.buildProducts()}
+			<div className="MyProductsList">
+				<div className="ProductsFilter">
+					<div className="ui input products-filter">
+						<input type="text" placeholder="Filter..." onKeyUp={this.setFilter}></input>
+					</div>
+				</div>
+				<div className="ProductsList">
+					{ this.state.productItems }
+				</div>
 			</div>
 		);
 	}
