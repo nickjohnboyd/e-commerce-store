84a535ee22b59195218bb56d2b01f041ef15c2b2
Nick Boyd <nickjohnboyd@gmail.com>
2023-03-24 16:39:09 -0600

    Deploy React app to GitHub Pages

---
+++ precache-manifest.6c5dddc0c56f4f2df9bfaa2c6d9aaf96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9230328bb39b9085186715e21dfdf7",
+    "url": "/e-commerce-store/index.html"
+  },
+  {
+    "revision": "321719f8e21782825a60",
+    "url": "/e-commerce-store/static/css/main.1b7dd74e.chunk.css"
+  },
+  {
+    "revision": "be3afce39403951b9adf",
+    "url": "/e-commerce-store/static/js/2.b506a541.chunk.js"
+  },
+  {
+    "revision": "84d3749cfda90e9ccba3e45a9761e851",
+    "url": "/e-commerce-store/static/js/2.b506a541.chunk.js.LICENSE"
+  },
+  {
+    "revision": "321719f8e21782825a60",
+    "url": "/e-commerce-store/static/js/main.e7cf3f6b.chunk.js"
+  },
+  {
+    "revision": "6a17b8bd2f4e28a21f52",
+    "url": "/e-commerce-store/static/js/runtime-main.2783665b.js"
+  },
+  {
+    "revision": "99f60a68aa6cab33c30869760bc9cbc8",
+    "url": "/e-commerce-store/static/media/macbook-pro-table.99f60a68.jpeg"
+  }
+]);
